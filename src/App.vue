7d53b4d0784c9ef807a89d27f7e467cdd5a3b0d9
gbmsaraujo <gbmsaraujo@gmail.com>
2022-07-27 09:13:42 -0300

    Configuração do Projeto

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+ <div>
+  <h1 class="titulo">{{title}}</h1>
+ </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+
+  },
+  data(){
+    return{
+      title:"Projeto Lanchonete Mentoria",
+    }
+  }
+
+}
+</script>
+
+<style scoped>
+  .titulo{
+    @apply text-lg bg-red-300 text-center p-4
+  }
+</style>

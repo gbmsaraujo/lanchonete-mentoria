b07ddd1a3ec8fcf83f87496f4484bc4ea5df43c6
Gabriel Araujo <gabriel.araujo@solfacil.com.br>
2022-07-27 12:28:38 -0300

    Organizando as estruturas do projeto

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,28 @@
 <template>
- <div>
-  <h1 class="titulo">{{title}}</h1>
- </div>
+	<h1 class="titulo">{{ title }}</h1>
+	<div id="ct-components">
+		<realizar-pedido />
+	</div>
 </template>
 
 <script>
+	import RealizarPedido from "./components/RealizarPedido.vue";
 
-export default {
-  name: 'App',
-  components: {
-
-  },
-  data(){
-    return{
-      title:"Projeto Lanchonete Mentoria",
-    }
-  }
-
-}
+	export default {
+		name: "App",
+		components: {
+			RealizarPedido
+		},
+		data() {
+			return {
+				title: "Projeto Lanchonete Mentoria"
+			};
+		}
+	};
 </script>
 
 <style scoped>
-  .titulo{
-    @apply text-lg bg-red-300 text-center p-4
-  }
+	.titulo {
+		@apply text-lg bg-red-300 text-center p-4;
+	}
 </style>
